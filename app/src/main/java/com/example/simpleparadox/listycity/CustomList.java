06bf488987613c79e0ddbcc97ddf7c2d3a0b5d89
Mehul Gadhia <mg@Mehuls-MacBook-Pro.local>
2022-03-11 14:35:24 -0700

    Has City test after feature is implemented

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -67,7 +67,6 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public boolean hasCity(City city) {
-        //return cities.contains(city);
-        return false;
+        return cities.contains(city);
     }
 }
